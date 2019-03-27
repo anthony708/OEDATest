163a9800a06c038ed7e47a29cf090467c6512453
anthony708 <anthony78@live.cn>
2019-03-27 17:42:57 +0800

    Xpath: locate more elements in navigation 'Users and groups'.

--- src/main/java/locators/Loc.java
+++ src/main/java/locators/Loc.java
@@ -5,6 +5,8 @@ public class Loc {
     // Navigation
     public static String hardwareItem = "//span[@class=\"oj-navigationlist-item-label\" and contains(text(), 'Hardware')]";
     public static String rackNetworksItem = "//span[@class=\"oj-navigationlist-item-label\" and contains(text(), 'Rack Networks')]";
+    public static String operatingSystemItem = "//span[@class=\"oj-navigationlist-item-label\" and contains(text(), 'Operating System')]";
+    public static String usersItem = "//span[@class=\"oj-navigationlist-item-label\" and contains(text(), 'Users')]";
 
     /*
         Hardware
@@ -36,6 +38,7 @@ public class Loc {
     public static String rackSizeDropList = "//*[@id=\"ojChoiceId_selectRackSize_selected\"]";
     public static String rackSizeElastic = "//div[@class='oj-listbox-result-label' and text()=\"Elastic\"]";
     public static String rackSizeFull = "//div[contains(text(),'Full')]";
+    public static String rackSizeQuarter = "//div[contains(text(),'Quarter')]";
 
     public static String diskSizeDropList = "//*[@id=\"ojChoiceId_selectCellServerCapacity_selected\"]";
     public static String diskSizeHC10TB = "//div[@class='oj-listbox-result-label' and contains(text(),'HC 10TB')]";
@@ -44,10 +47,10 @@ public class Loc {
     public static String addBtn = "//button[@id=\"addRackBtn\"]";
 
     public static String rackName = "/html/body/div[3]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[5]/div/div[4]/div/div[3]/div[1]/div/label";
-    public static String rackPrefixText = inputAltXPath("Rack Prefix");
+    public static String rackPrefixInput = inputAltXPath("Rack Prefix");
     public static String spineSwitchCheckbox = inputAltXPath("Spine Switch");
-    public static String customerNameText = inputAltXPath("Customer Name");
-    public static String appSerialNumText = inputAltXPath("Application/Serial #");
+    public static String customerNameInput = inputAltXPath("Customer Name");
+    public static String appSerialNumInput = inputAltXPath("Application/Serial #");
 
     public static String regionLabel = "//div[@class='oj-label-group']/label[contains(text(),'Region')";
     public static String regionDropList = "//div[@class='oj-label-group']/label[contains(text(),'Region')]/../../following-sibling::div";
@@ -69,14 +72,14 @@ public class Loc {
     public static String timezoneSearchBtn = "//span[@class=\"oj-listbox-spyglass-box\"]";
     public static String timezoneNY = "//div[@class='oj-listbox-result-label' and @aria-label='New_York']";
 
-    public static String computeNodeCountText = inputAltXPath("Compute Node count");
-    public static String storageNodeCountText = inputAltXPath("Storage Node count");
-    public static String DNSServers1Text = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../following-sibling::div/input[@alt='DNS Servers']";
-    public static String DNSServers2Text = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../../following-sibling::div[1]/div/input[@alt='DNS Servers']";
-    public static String DNSServers3Text = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../../following-sibling::div[2]/div/input[@alt='DNS Servers']";
-    public static String NTPServers1Text = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../following-sibling::div/input[@alt='NTP Servers']";
-    public static String NTPServers2Text = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../../following-sibling::div[1]/div/input[@alt='NTP Servers']";
-    public static String NTPServers3Text = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../../following-sibling::div[2]/div/input[@alt='NTP Servers']";
+    public static String computeNodeCountInput = inputAltXPath("Compute Node count");
+    public static String storageNodeCountInput = inputAltXPath("Storage Node count");
+    public static String dnsServers1Input = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../following-sibling::div/input[@alt='DNS Servers']";
+    public static String dnsServers2Input = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../../following-sibling::div[1]/div/input[@alt='DNS Servers']";
+    public static String dnsServers3Input = "//div[@class='oj-label-group']/label[contains(text(), 'DNS Servers')]/../../../following-sibling::div[2]/div/input[@alt='DNS Servers']";
+    public static String ntpServers1Input = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../following-sibling::div/input[@alt='NTP Servers']";
+    public static String ntpServers2Input = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../../following-sibling::div[1]/div/input[@alt='NTP Servers']";
+    public static String ntpServers3Input = "//div[@class='oj-label-group']/label[contains(text(), 'NTP Servers')]/../../../following-sibling::div[2]/div/input[@alt='NTP Servers']";
 
     public static String automcticWBFCIcon = "//input[@value=\"automatic\"]";
     public static String disableWBFCIcon = "//input[@value=\"disable\"]";
@@ -88,24 +91,55 @@ public class Loc {
     public static String confirmedBtn = "// button[@id=\"codConfirmButton\"]";
 
     /*
+        Operating System
+     */
+    public static String osChoiceDropList = "//*[@id=\"ojChoiceId_selectOsChoice_selected\"]";
+    public static String osCLinuxOVM = "//*[@id=\"ojChoiceId_selectOsChoice_selected\"]";
+    public static String osCLinuxPhysical = "//div[@class='oj-listbox-result-label' and @aria-label='All Linux Physical']";
+    public static String osCCustom = "//div[@class='oj-listbox-result-label' and @aria-label='Custom']";
+    public static String osSaveBtn = "//*[@id=\"osSaveBtn\"]";
+
+    /*
         Rack Networks
       */
     public static String removePDUsCheckBox = "/html/body/div[3]/div/div[2]/div/div[2]/div/div/div[3]/div/div/div/div[2]/div[1]/div/input[@type='checkbox']";
     public static String rackNwAdvBtn = "//button[@id=\"RackNwAdvancedButton\"]";
     public static String gateWayCheckBox = "//input[@alt='Default Gateway']";
-    public static String gateWayText = inputAltXPath("Gateway");
-    public static String startIPAddressText = inputAltXPath("Start IP");
-    public static String domainNameText = inputAltXPath("Domain Name");
-    public static String seperateILOMNwCheckBox = "//input[@title='Separate ILOM network']";
-    public static String modifyMasksBtn = "//*[@id=\"buttonOpener\"]";
+    public static String gateWayInput = inputAltXPath("Gateway");
+    public static String anStartIPAddress = inputAltXPath("Start IP");
 
-    public static String adminNetworkApplyBtn = "//button[@id=\"saveRackNetworksButtonAdmin\"]";
+    public static String anSubnetMaskDropList = "//label[contains(text(),'Subnet Mask')]/../../following-sibling::div/div";
+
+    public static String anSubnetMaskSearchField = "/html/body/div[1]/div/div/div/div/input";
+    public static String anSubnetMask2552552400 = "//div[@aria-label='255.255.240.0']";
+
+    public static String anDomainNameInput = "//input[@alt=\"Domain Name\"]";
+
+    public static String seperateILOMNwCheckBox = "//input[@title='Separate ILOM network']";
+    public static String anModifyMasksBtn = "//*[@id=\"buttonOpener\"]";
+    public static String adminNetworkApplyBtn = "//*[@id=\"saveRackNetworksButtonAdmin\"]";
+
+    public static String pnStartIPAddress = "//label[contains(text(),'Start IP Address')]/../../following-sibling::div/input[@alt=\"Start IP Address\"]";
+    //public static String pnSubnetMaskDropList = "";
+    public static String pnSubnetMaskSearchField = "";
+    public static String pnSubnetMask2552552480 = "";
+    //public static String pnDomainNameInput = "";
+    public static String privateNetworkApplyBtn = "//*[@id=\"saveRackNetworksButtonPrivate\"]";
 
     /*
-    
+        Users
      */
 
-    public static String inputAltXPath(String alt) {
+    public static String defaultRadioBtn = "//span[contains(text(),'Default')]/../preceding-sibling::span";
+    public static String roleSeperatedRadioBtn = "//span[contains(text(),'Role Separated')]/../preceding-sibling::span";
+    public static String customRadioBtn = "//span[contains(text(),'Custom')]/../preceding-sibling::span";
+
+
+    /*
+        Clusters
+     */
+
+    private static String inputAltXPath(String alt) {
         return "//input[@alt=\"" + alt + "\"]";
     }
 
